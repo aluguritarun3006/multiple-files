30d99715b4c8c69613ac98806a03bc8629429502
aluguritarun3006 <99875462+aluguritarun3006@users.noreply.github.com>
2022-04-12 15:29:50 +0530

    Update test1.tf

--- test1.tf
+++ test1.tf
@@ -1,1 +1,9 @@
-h
+resource "aws_s3_bucket" "tarun-bucket" {
+  bucket = "my-tarun-test-bucket" ,,
+  acl    = "private"
+
+  tags = {
+    Name        = "My tarun bucket"
+    Environment = "Dev"
+  }
+}
